9c51208b3f5feac8bcd8508fed385e23ca13263b
Ariel Danara <81428484+Arieldanar@users.noreply.github.com>
2021-04-03 20:04:37 +0700

    Add files via upload

---
+++ assets/effect.js
@@ -0,0 +1,6 @@
+const bars = document.querySelector('.bars');
+const navigasi2 = document.querySelector('.navigasi2');
+
+bars.addEventListener('click', function() {
+	navigasi2.classList.toggle('slide');
+});
